82776b19f8d7f3891083a88b2b8311d845abc642
JohnnyPP <johnnnyppp@gmail.com>
2014-02-23 22:32:13 +0100

    Sampling without function

--- mcp3004.py
+++ mcp3004.py
@@ -9,19 +9,20 @@ spi.open(0,0)
 numberOfSamplesToAcquire = 10000
 ADCdata = []
 AcquiringTime = []
+channel = 0
 
-def get_adc(channel):
+#def get_adc(channel):
     #Perform SPI transaction and store returned bits in 'r'
-    r = spi.xfer([1, (8+channel)<<4, 0])
+    #r = spi.xfer([1, (8+channel)<<4, 0])
     #Filter data bits from returned bits
-    adcout = ((r[1]&3) << 8) + r[2]
+    #adcout = ((r[1]&3) << 8) + r[2]
     #Return value from 0-1023
-    return adcout
+    #return adcout
 
 for i in range(10):   
     startTimer = time.clock()
     for i in range(numberOfSamplesToAcquire):
-        ADCdata.append(get_adc(0))
+        ADCdata.append((((spi.xfer([1, (8+channel)<<4, 0]))[1]&3) << 8) + (spi.xfer([1, (8+channel)<<4, 0]))[2])
     
     AcquiringTime.append(time.clock() - startTimer)
 
@@ -30,9 +31,3 @@ meanTime = np.mean(AcquiringTime)
 print 'Mean ADC sampling time of', numberOfSamplesToAcquire, 'samples:', "{0:.4f}".format(meanTime), \
 '[s]' '\nStandard deviation:', "{0:.4f}".format(np.std(AcquiringTime)), '[s]' \
 '\nNumber of samples pro second:', numberOfSamplesToAcquire/meanTime, '[S/s]'
-
-
-
-#while True:
-    #print get_adc(0)
-    #time.sleep(1)
