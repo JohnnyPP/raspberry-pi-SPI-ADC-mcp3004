b16346071d2b9f7c6b97780775c8ffd410445626
JohnnyPP <johnnnyppp@gmail.com>
2014-02-26 00:51:26 +0100

    Implemented queue

--- mcp3004.py
+++ mcp3004.py
@@ -1,12 +1,14 @@
 import spidev
 import time
 import numpy as np
+from threading import Thread
+from Queue import Queue
 
+queue = Queue(10)
 
-#Establish SPI connection with Bus 0, Device 0
 spi = spidev.SpiDev()
 spi.open(0,0)
-numberOfSamplesToAcquire = 10000
+numberOfSamplesToAcquire = 9
 ADCdata = []
 AcquiringTime = []
 
@@ -17,16 +19,33 @@ def get_adc(channel):
     adcout = ((r[1]&3) << 8) + r[2]
     #Return value from 0-1023
     return adcout
-
-for i in range(10):   
-    startTimer = time.clock()
-    for i in range(numberOfSamplesToAcquire):
-        ADCdata.append(get_adc(0))
     
-    AcquiringTime.append(time.clock() - startTimer)
+class ProducerThread(Thread):
+    def run(self):
+        global queue      
+        for i in range(numberOfSamplesToAcquire):
+            queue.put(get_adc(0))
+            
+class ConsumerThread(Thread):
+    def run(self):
+        global queue   
+        for i in range(numberOfSamplesToAcquire):
+            ADCdata.append(queue.get())
 
-meanTime = np.mean(AcquiringTime)
 
-print 'Mean ADC sampling time of', numberOfSamplesToAcquire, 'samples:', "{0:.4f}".format(meanTime), \
-'[s]' '\nStandard deviation:', "{0:.4f}".format(np.std(AcquiringTime)), '[s]' \
-'\nNumber of samples pro second:', numberOfSamplesToAcquire/meanTime, '[S/s]'
+ProducerThread().start()
+ConsumerThread().start()
+
+print 'Done!'
+#for i in range(10):   
+#    startTimer = time.clock()
+#    for i in range(numberOfSamplesToAcquire):
+#        ADCdata.append(get_adc(0))
+    
+#    AcquiringTime.append(time.clock() - startTimer)
+
+#meanTime = np.mean(AcquiringTime)
+
+#print 'Mean ADC sampling time of', numberOfSamplesToAcquire, 'samples:', "{0:.4f}".format(meanTime), \
+#'[s]' '\nStandard deviation:', "{0:.4f}".format(np.std(AcquiringTime)), '[s]' \
+#'\nNumber of samples pro second:', numberOfSamplesToAcquire/meanTime, '[S/s]'
