1e6b857a84d3b62e11510ac8dd874292f27e6ee5
JohnnyPP <johnnnyppp@gmail.com>
2014-03-02 01:43:48 +0100

    Added time.time() for measuring time

--- mcp3004.py
+++ mcp3004.py
@@ -5,9 +5,11 @@ import numpy as np
 #Establish SPI connection with Bus 0, Device 0
 spi = spidev.SpiDev()
 spi.open(0,0)
-numberOfSamplesToAcquire = 1000
-AcquiringTime = 0
+numberOfSamplesToAcquire = 100
+AcquiringTime = 1
 ADCdata = []
+i = 0
+startTimer = time.clock()
 
 def get_adc(channel):
     #Perform SPI transaction and store returned bits in 'r'
@@ -19,12 +21,21 @@ def get_adc(channel):
 
 print 'Starting acquiring', numberOfSamplesToAcquire, 'samples.'
 
-startTimer = time.clock()
+startTimer = time.time()
+
+#while i<numberOfSamplesToAcquire:
+#    ADCdata.append(get_adc(0))
+#    print get_adc(0)
+#    i=i+1
+#    time.sleep(0.01)  
+#AcquiringTime = (time.time() - startTimer)
+
+startTimer = time.time()
 for i in range(numberOfSamplesToAcquire):
     ADCdata.append(get_adc(0))
-    time.sleep(0.001)
-    
-AcquiringTime = (time.clock() - startTimer)
+    time.sleep(0.01)    
+AcquiringTime = (time.time() - startTimer)
 
-print 'ADC sampling time of', numberOfSamplesToAcquire, 'samples:', AcquiringTime, \
-'\nNumber of samples pro second:', numberOfSamplesToAcquire/AcquiringTime, '[S/s]'
+print 'ADC sampling time of', numberOfSamplesToAcquire, 'samples:', AcquiringTime,
+print '\nNumber of samples pro second:', numberOfSamplesToAcquire/AcquiringTime, '[S/s]'
+#print ADCdata[0]
