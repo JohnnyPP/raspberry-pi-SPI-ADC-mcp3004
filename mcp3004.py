f89a54c291162a3c11c6168021f8c79d586c2bad
JohnnyPP <johnnnyppp@gmail.com>
2014-03-02 14:56:49 +0100

    Queue with time measurement

--- mcp3004.py
+++ mcp3004.py
@@ -2,15 +2,15 @@ import spidev
 import time
 import numpy as np
 from threading import Thread
+import threading
 from Queue import Queue
 
 queue = Queue(10)
-
 spi = spidev.SpiDev()
 spi.open(0,0)
-numberOfSamplesToAcquire = 9
+numberOfSamplesToAcquire = 100
 ADCdata = []
-AcquiringTime = []
+AcquiringTime = 0
 
 def get_adc(channel):
     #Perform SPI transaction and store returned bits in 'r'
@@ -24,28 +24,20 @@ class ProducerThread(Thread):
     def run(self):
         global queue      
         for i in range(numberOfSamplesToAcquire):
-            queue.put(get_adc(0))
+            queue.put(get_adc(0))         
+            time.sleep(0.00579)
             
 class ConsumerThread(Thread):
     def run(self):
-        global queue   
+        global queue 
+        startTimer = time.time()
         for i in range(numberOfSamplesToAcquire):
             ADCdata.append(queue.get())
+        AcquiringTime = (time.time() - startTimer)
+        print 'ADC sampling time: ', AcquiringTime
+        print 'Number of samples per second:', numberOfSamplesToAcquire/AcquiringTime, '[S/s]'
 
-
-ProducerThread().start()
-ConsumerThread().start()
-
-print 'Done!'
-#for i in range(10):   
-#    startTimer = time.clock()
-#    for i in range(numberOfSamplesToAcquire):
-#        ADCdata.append(get_adc(0))
-    
-#    AcquiringTime.append(time.clock() - startTimer)
-
-#meanTime = np.mean(AcquiringTime)
-
-#print 'Mean ADC sampling time of', numberOfSamplesToAcquire, 'samples:', "{0:.4f}".format(meanTime), \
-#'[s]' '\nStandard deviation:', "{0:.4f}".format(np.std(AcquiringTime)), '[s]' \
-#'\nNumber of samples pro second:', numberOfSamplesToAcquire/meanTime, '[S/s]'
+if __name__ == "__main__":
+    print 'Starting acquiring', numberOfSamplesToAcquire, 'samples.'
+    ProducerThread().start()
+    ConsumerThread().start()
